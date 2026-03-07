73f5226abdd7542a8c4810a4a5b61be3b55a2a62
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-03-08 01:21:29 +0700

    style: fomated project, rework to 2 space indentation

--- js/preloader.js
+++ js/preloader.js
@@ -1,5 +1,5 @@
-const preloader = document.querySelector('.preloder');
-preloader.classList.add('active');
+const preloader = document.querySelector(".preloder");
+preloader.classList.add("active");
 setTimeout(() => {
-  preloader.classList.remove('active');
-}, 500)
+  preloader.classList.remove("active");
+}, 500);
