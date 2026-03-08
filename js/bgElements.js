c550204cf8c89d4a5b4f7d029324ac77532001be
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-03-08 17:04:08 +0700

    feat: complete l03

--- js/bgElements.js
+++ js/bgElements.js
@@ -1,10 +1,11 @@
 const bgElements = () => {
-  const elements = document.querySelectorAll(".set-bg");
+  setTimeout(() => {
+    const elements = document.querySelectorAll(".set-bg");
 
-  for (let i = 0; i < elements.length; i++) {
-    const src = elements[i].dataset.setbg;
-    elements[i].style.backgroundImage = `url(${src})`;
-  }
+    elements.forEach((element) => {
+      element.style.backgroundImage = `url(${element.dataset.setbg})`;
+    });
+  }, 0);
 };
 
 bgElements();
--- js/mainData.js
+++ js/mainData.js
@@ -1,6 +1,7 @@
 const mainData = () => {
   const url = "https://glo-anime-js-default-rtdb.firebaseio.com/anime.json";
   const localUrl = "./db.json";
+
   fetch(url)
     .then((response) => {
       if (!response.ok) {
@@ -9,7 +10,7 @@ const mainData = () => {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      processData(data);
       return data;
     })
     .catch((error) => {
@@ -22,13 +23,55 @@ const mainData = () => {
           return response.json();
         })
         .then((localData) => {
-          console.log(localData);
-          return localData;
+          processData(localData.anime);
+          return localData.anime;
         })
         .catch((error) => {
           console.log(error);
         });
     });
 };
+
+function processData(data) {
+  const ganres = new Set();
+  renderTopAnime(data.sort((a, b) => b.views - a.views).slice(0, 5));
+  data.forEach((item) => {
+    ganres.add(item.ganre);
+  });
+  renderAnimeList(data, ganres);
+}
+
+function renderTopAnime(array) {
+  const wrapper = document.querySelector(".filter__gallery");
+  wrapper.innerHTML = "";
+  array.forEach((element) => {
+    wrapper.insertAdjacentHTML(
+      "afterbegin",
+      `
+      <div
+        class="product__sidebar__view__item set-bg mix"
+        data-setbg="${element.image}"
+      >
+        <div class="ep">${element.rating} / 10</div>
+        <div class="view"><i class="fa fa-eye"></i> ${element.views}</div>
+        <h5>
+          <a href="/anime-details.html"
+            >${element.title}</a
+          >
+        </h5>
+      </div>
+      `,
+    );
+    wrapper.querySelectorAll(".set-bg").forEach((element) => {
+      element.style.backgroundImage = `url(${element.dataset.setbg})`;
+    });
+  });
+}
+function renderAnimeList(array, ganres) {
+  console.log(array);
+  console.log(ganres);
+  
+
+}
 
 mainData();
